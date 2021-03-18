56269a6d91c5ba238766c8997463f6509619acd3
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-18 22:36:27 +0530

    Move cart and shop cards to cart and shop from App

--- src/Components/main components/Cart.js
+++ src/Components/main components/Cart.js
@@ -1,27 +1,39 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import CartItemCard from "../render components/CartItemCard";
 
 function Cart(props) {
-    const {cartItems, cartItemCards} = props;
-    const total = cartItems.reduce((acc, cur) => acc + (cur.quantity * cur.price), 0)
 
-    if(Array.isArray(cartItems) && !cartItems.length) {
-        return(
-            <div>
-                <h3>Cart is empty</h3>
-            </div>
-        )
-    }
+  const {cartItems, cartItemCards} = props;
+
+  const cartItemCards = cartItemsArray.map(cartItem => <CartItemCard
+    key={cartItem.id}
+    values={cartItem}
+    cartItems={cartItemsArray}
+    updateCart={updateCart}
+  />);
+
+  const total = cartItems.reduce((acc, cur) => acc + (cur.quantity * cur.price), 0)
+
+  if (Array.isArray(cartItems) && !cartItems.length) {
     return (
-        <div>
-            <h1>This is cart</h1>
-            <div className="cartList">
-                {cartItemCards}
-            </div>
-            <p>Grand total is {total}</p>
-            <Link to="buy"><button>Proceed to Buy</button></Link>
-        </div>
+      <div>
+        <h3>Cart is empty</h3>
+      </div>
     )
+  }
+  return (
+    <div>
+      <h1>This is cart</h1>
+      <div className="cartList">
+        {cartItemCards}
+      </div>
+      <p>Grand total is {total}</p>
+      <Link to="buy">
+        <button>Proceed to Buy</button>
+      </Link>
+    </div>
+  )
 }
 
 export default Cart;
--- src/Components/main components/Shop.js
+++ src/Components/main components/Shop.js
@@ -1,28 +1,42 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import ShopItemCard from "../render components/ShopItemCard";
 
 function Shop(props) {
-    const {cartItems, shopItems} = props;
-    if (Array.isArray(cartItems) && cartItems.length) {
-        return(
-            <div className="shop">
-                <h1>this is shop</h1>
-                <div>
-                    {shopItems}
-                </div>
-                <Link to="/cart"><button>Go to cart</button></Link>
-                <Link to="/buy"><button>Proceed to buy</button></Link>
-            </div>
-        )
-    }
+  const {cartItems, shopItems} = props;
+
+  const shopItemCards = shopItemsArray.map(shopItem => <ShopItemCard
+    key={shopItem.id}
+    values={shopItem}
+    cartItems={cartItemsArray}
+    updateCart={updateCart}
+    addToCart={addToCart}
+  />);
+
+  if (Array.isArray(cartItems) && cartItems.length) {
     return (
-        <div className="shop">
-            <h1>this is shop</h1>
-            <div>
-                {shopItems}
-            </div>
+      <div className="shop">
+        <h1>this is shop</h1>
+        <div>
+          {shopItems}
         </div>
+        <Link to="/cart">
+          <button>Go to cart</button>
+        </Link>
+        <Link to="/buy">
+          <button>Proceed to buy</button>
+        </Link>
+      </div>
     )
+  }
+  return (
+    <div className="shop">
+      <h1>this is shop</h1>
+      <div>
+        {shopItems}
+      </div>
+    </div>
+  )
 
 }
 
