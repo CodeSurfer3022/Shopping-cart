d40a01c7a2a6c610acd6afcfcd4bf33464630376
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-23 18:50:42 +0530

    Add address summary

--- src/Components/main components/PlaceOrder.js
+++ src/Components/main components/PlaceOrder.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import OrderSummary from "../summary components/OrderSummary";
 import PaymentSummary from "../summary components/PaymentSummary";
+import AddressSummary from "../summary components/AddressSummary";
 
 function PlaceOrder() {
   return (
@@ -9,13 +10,11 @@ function PlaceOrder() {
       <Link to="/payment">
         <i className="fas fa-arrow-left"></i>
       </Link>
+
       <h2>Place your order</h2>
       <OrderSummary />
       <PaymentSummary />
-      <div className="addressSummary">
-      </div>
-      <div className="ItemsSummary">
-      </div>
+      <AddressSummary />
 
       <Link to="/cart">
         <p>Cancel and go to cart</p>
--- src/Components/summary components/AddressSummary.js
+++ src/Components/summary components/AddressSummary.js
@@ -3,7 +3,8 @@ import React from "react";
 function AddressSummary() {
   return(
     <div>
-
+      <h3>Deliver to</h3>
+      <p>address</p>
     </div>
   )
 }
