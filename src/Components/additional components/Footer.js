4c598212165c0df1df2e98baf2ee0d039ae781db
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-31 20:11:10 +0530

    Style footer

--- src/Components/additional components/Footer.js
+++ src/Components/additional components/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './Footer.css'
 
 function Footer() {
     return(
         <footer>
-            <p>About us</p>
+          <p>Hosted with love by <a href="https://github.com/chandraprakash-dev">github</a></p>
+          <a href="https://github.com/chandraprakash-dev/Shopping-cart">
+            <i className="fas fa-code fa-2x"></i>
+          </a>
         </footer>
     )
 }
