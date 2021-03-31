e6f70599c5ab7ec3919dc876913e4fad41103521
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-31 22:18:51 +0530

    Style home

--- src/Components/main components/Home.js
+++ src/Components/main components/Home.js
@@ -3,14 +3,15 @@ import {Link} from "react-router-dom";
 import './Home.css'
 
 function Home() {
-    return(
-        <div className="home">
-            <h2>Are you a Knight? Welcome to Fort night</h2>
-            <Link to="/shop">
-              <button>Purchase artefacts</button>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="home">
+      <h2>Are you a Knight?</h2>
+      <h2>Welcome to Fort night!</h2>
+      <Link to="/shop">
+        <button>Purchase artefacts</button>
+      </Link>
+    </div>
+  )
 }
 
 export default Home;
