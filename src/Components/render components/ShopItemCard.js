c7e667ac145c3f7b4e404ba1d14c5d6d4a63d3cd
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-04-17 19:24:22 +0530

    display more info on shop page list

--- src/Components/render components/ShopItemCard.js
+++ src/Components/render components/ShopItemCard.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 
 function ShopItemCard(props) {
+  const [item ,setItem] = useState();
+  useEffect(() => {
+    fetchItem();
+
+  }, [])
+
   const cartItems = props.cartItems;
   const id = props.id;
 
@@ -14,12 +20,36 @@ function ShopItemCard(props) {
     <button onClick={() => props.updateCart(itemIndex, '+')}>+</button>
   </div> : <button onClick={() => props.addToCart(id)}>Add to cart</button>;
 
+  const fetchItem = async (id) => {
+    const data = await fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${id}`, {mode: 'cors'});
+    const response = await data.json();
+    const item = response.data;
+
+    console.log(item);
+    setItem(item);
+  }
+
+  if(!item) {
+    return(
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="shop-card">
       <Link to={`shop/${id}`}>
         <p>{props.values.name}</p>
       </Link>
       <p>{props.values.cost}</p>
+      <h1>{item.item.name}</h1>
+      <img src={item.item.images.icon} alt={item.name}/>
+      <div>
+        <p>{item.item.rarity}</p>
+        <p>{item.item.ratings.avgStars}</p>
+        <p>{item.item.ratings.numberVotes} ratings</p>
+      </div>
       {quantityComponent}
     </div>
   )
